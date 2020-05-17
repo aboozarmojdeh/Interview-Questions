5228228884def3bd68a795f10f3311c66fdcb49c
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2020-05-17 14:02:01 -0400

    anagrams-method2

---
+++ anagramsMethod2.js
@@ -0,0 +1,30 @@
+// Anagrams – Method 2 - Character Map
+
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
+
+
